e8c7c0d01c2f514582360b09c21c3af0e6ba1809
David <david.klapsia@hotmail.fr>
2020-03-24 20:24:18 +0100

    version 1

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,22 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyABq2uAd1AerBWdhoAudZtaBrUVzjQAF_A",
+    authDomain: "awesome-list-5ad9d.firebaseapp.com",
+    databaseURL: "https://awesome-list-5ad9d.firebaseio.com",
+    projectId: "awesome-list-5ad9d",
+    storageBucket: "awesome-list-5ad9d.appspot.com",
+    messagingSenderId: "123948106430",
+    appId: "1:123948106430:web:0ede6d181d7c776f0b94ef",
+    auth: {
+			baseURL: 'https://www.googleapis.com/identitytoolkit/v3/relyingparty'
+		},
+    firestore: {
+			baseURL : 'https://firestore.googleapis.com/v1/projects/awesome-list-5ad9d/databases/(default)/documents'
+		}
+  }
 };
 
 /*
